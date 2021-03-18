4691f020b08944e91508741055a856d4c5d04bc4
joe <joelibral2693@gmail.com>
2021-03-18 16:38:16 +0330

    filtering schema

--- comments/schema.py
+++ comments/schema.py
@@ -8,6 +8,13 @@ class CommentType(DjangoObjectType):
 
 class CommentQuery(ObjectType):
     comments = graphene.List(CommentType)
+    comment = graphene.Field(CommentType, id=graphene.Int())
 
+    def resolve_comment(parent, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Comment.objects.get(id=id)
+        return None
+        
     def resolve_comments(parent, info, **kwargs):
         return Comment.objects.all()
--- posts/schema.py
+++ posts/schema.py
@@ -7,16 +7,37 @@ class UserType(DjangoObjectType):
     class Meta:
         model = User
 
-class PostTyppe(DjangoObjectType):
+class PostsTyppe(DjangoObjectType):
     class Meta:
         model = Post
 
 class PostsQuery(ObjectType):
-    posts = graphene.List(PostTyppe)
-    user = graphene.List(UserType)
+    posts = graphene.List(PostsTyppe)
+    users = graphene.List(UserType)
+    post = graphene.Field(PostsTyppe, id=graphene.Int())
+    user = graphene.Field(UserType, username=graphene.String())
+    postuser = graphene.List(PostsTyppe, name=graphene.String())
+
+    def resolve_postuser(parent, info, **kwargs):
+        name = kwargs.get('name')
+        if name is not None:
+            return Post.objects.filter(user__username=name)
+        return None
 
     def resolve_posts(parent, info, **kwargs):
         return Post.objects.all() 
 
-    def resolve_user(parent, info, **kwargs):
+    def resolve_users(parent, info, **kwargs):
         return User.objects.all()
+
+    def resolve_post(parent, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Post.objects.get(id=id)
+        return None
+
+    def resolve_user(parent, info, **kwargs):
+        username = kwargs.get('username')
+        if username is not None:
+            return User.objects.get(username=username)
+        return None
